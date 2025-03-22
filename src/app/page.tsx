47c1c809988648e77acb5b2555af68c5c1951c93
tahmedofficial <tanvirofcuo@gmail.com>
2025-03-22 16:42:36 +0600

    added custom button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import HeroSection from "@/components/ui/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <h1>chai</h1>
+      <HeroSection></HeroSection>
     </main>
   );
 }
